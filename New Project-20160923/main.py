e37fd0a77887fa264348a08ce51ff570343fd2e9
junyeopN <junyeop0902@snu.ac.kr>
2016-09-23 14:34:38 +0900

    Created by Coding Ground Client

---
+++ New Project-20160923/main.py
@@ -0,0 +1,10 @@
+# Hello World program in Python
+    
+print "Hello World!\n"
+
+class Batter
+    def __init__(self, name, batting_average, homeruns)
+        self.name = name;
+        self.batting_average = batting_average;
+        self.homeruns = homeruns;
+    
